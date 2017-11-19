b3dd94449bb45959251877211e791038ee51ff08
Pleshcheev Alexander <al39pl@gmail.com>
2017-11-19 21:45:28 +0300

    uploaded part of hw 5

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import app from "./app"
 
 const cookieParser = require("cookie-parser");
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 const _ = require("underscore");
 const expressQSParser = require('express-qs-parser');
 const port = process.env.PORT || 8080;
 
+app.use(bodyParser.json())
+app.use(cookieParser());
+
+let qsParserMiddleware = expressQSParser({
+    params: {
+        filters: /([\w-_]+)(\>|<|\=|\!=)([\w_-]+)/g,
+        order: /(-?)([\w\s]+)/
+    },
+    storage: 'parsedQuery'
+});
+app.use(qsParserMiddleware);
 
 let products = [
     {
@@ -25,18 +38,27 @@ let products = [
     }
 ]
 
+let users = {
+    "user1": {
+        password: "123",
+        username: "elUser",
+        email: "elEmail"
+    },
+    "user2": {
+        password: "456",
+        username: "elOtroUser",
+        email: "elOtroEmail"
+    },
+    "user3": {
+        password: "789",
+        username: "aquelUser",
+        email: "aquelEmail"
+    }
+}
+
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
-let qsParserMiddleware = expressQSParser({    
-    params: {     
-        filters: /([\w-_]+)(\>|<|\=|\!=)([\w_-]+)/g, 
-        order: /(-?)([\w\s]+)/
-    },
-    storage: 'parsedQuery' 
-});
-app.use(cookieParser());
-app.use(qsParserMiddleware);
 app.get("/", (req, res)=>{
     console.log("Cookies: ", req.cookies)
     res.status(200).json(req.parsedQuery);
@@ -83,4 +105,32 @@ app.post('/api/products', (req, res)=>{
     res.status(204).send()
 
     res.json({ok: true})
+})
+
+app.post('/auth', (req, res)=>{
+    let user = req.body;
+
+    if(users[user.name].password === user.password){
+
+        res.status(200).json(
+            {
+                "code": 200,
+                "message": "OK",
+                "data":{
+                    "user": {
+                        "email": users[user.name].username,
+                        "username": users[user.name].email
+                    }
+                },
+                "token": jwt.sign(user, '123456', { expiresIn: 60 * 60 })
+            }
+        ).send()
+    }
+    else{
+        res.status(404).json({
+            "code": 404,
+            "message": "Not found",
+            "data": {}
+        }).send()
+    }
 })
